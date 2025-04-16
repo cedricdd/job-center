7bb70d209daeeadb8189a09fb15e7736bb6a5b4d
Cedric Den Drijver <c-d-d@hotmail.fr>
2025-04-16 15:15:30 +0200

    still doing it

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -20,6 +20,7 @@ class JobFactory extends Factory
     public function definition(): array
     {
         $date = new DateTime();
+        $date->setTimezone(new \DateTimeZone('UTC'));
         $date->modify('-' . random_int(1, 60*60*30) . ' minutes')->format("Y-m-d H:i:s");
 
         return [
