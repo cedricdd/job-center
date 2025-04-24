00336a19861fc40c55dc9d5ce3249cce67e1849a
Cedric Den Drijver <c-d-d@hotmail.fr>
2025-04-24 18:36:36 +0200

    typo

--- resources/views/Components/flash-message.blade.php
+++ resources/views/Components/flash-message.blade.php
@@ -3,7 +3,7 @@
 @php
     $classes = "flex items-center p-4 m-2 text-sm border rounded-lg bg-red-50";
 
-    $classes .= $name == "failuer" ? " text-red-800 border-red-300 bg-red-50" : " text-green-800 border-green-300 bg-green-50";
+    $classes .= $name == "failure" ? " text-red-800 border-red-300 bg-red-50" : " text-green-800 border-green-300 bg-green-50";
 @endphp
 
 @if (session($name))
